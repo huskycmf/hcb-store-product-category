4702a12fb4be82bd3825702d703bce48424a3103
Oleksii Mylotskyi <spalaxinco@gmail.com>
2015-01-08 02:50:54 +0200

    Fixed bug with missed parent call.

--- src/HcbStoreProductCategory/InputFilter/Page.php
+++ src/HcbStoreProductCategory/InputFilter/Page.php
@@ -5,6 +5,8 @@ class Page extends \HcBackend\InputFilter\Page
 {
     public function __construct()
     {
+        parent::__construct();
+
         $this->add(array(
             'name' => 'pageContent',
             'allow_empty' => true
